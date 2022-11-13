80e6591fa62492495293b1b6a715394e0d1c6e78
Jakob Willforss <jakob.willforss@hotmail.com>
2022-11-13 21:16:09 +0100

    Laborating with getting flutter JS running

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# flutter_d3
 
-A new Flutter project.
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
+# Inspirations
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+* Running static HTML + javascript: https://itnext.io/load-local-html-file-into-webview-in-flutter-call-js-function-from-flutter-2d9404c8f7d4
--- lib/testwebview.dart
+++ lib/testwebview.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_js/flutter_js.dart';
 
 class HelpScreen extends StatefulWidget {
   @override
@@ -13,16 +14,59 @@ class HelpScreen extends StatefulWidget {
 
 class HelpScreenState extends State<HelpScreen> {
   late WebViewController _controller;
+  String filePath = 'assets/html/help.html';
+  late JavascriptRuntime flutterJs;
+  String _jsResult = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    flutterJs = getJavascriptRuntime();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Help')),
-      body: WebView(
-        initialUrl: 'about:blank',
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller = webViewController;
-          _loadHtmlFromAssets();
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('JS Evaluate Result: $_jsResult\n'),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                  'Click on the big JS Yellow Button to evaluate the expression bellow using the flutter_js plugin'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Math.trunc(Math.random() * 100).toString();",
+                style: TextStyle(
+                    fontSize: 12,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () async {
+          try {
+            JsEvalResult jsResult = flutterJs
+                .evaluate("Math.trunc(Math.random() * 100).toString();");
+            setState(() {
+              _jsResult = jsResult.stringResult;
+            });
+          } on PlatformException catch (e) {
+            print('ERRO: ${e.details}');
+          }
         },
       ),
     );
